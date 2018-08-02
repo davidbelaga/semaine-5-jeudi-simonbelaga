77ab0e0da0f0766c570f6e1916516adb68ae1e6c
SimonLembeye <simon.lembeye@live.com>
2018-08-02 22:40:35 +0200

    Ouiouioui

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash[:success] = "Vous êtes bien connecté"
       redirect_to '/'
     else
-      flash.now[:danger] = "Email/mot de passe invalide"
+      flash.now[:danger] = "Email / mot de passe invalide"
       render 'new'
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     @current_user = nil
     redirect_to root_url
   end
