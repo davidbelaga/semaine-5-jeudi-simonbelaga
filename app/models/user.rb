d0f6972b36b7b37e349513f3107134d3d6a80d0e
SimonLembeye <simon.lembeye@live.com>
2018-08-02 10:24:04 +0200

    deux

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+end
